b69f711c3db97f39b2695b99e6bc3cb01350ef60
anushanramesh <anushan@globalwavenet.com>
2019-07-25 15:38:47 +0530

    Generalized the clickhouse class by removing its ip address from each function

--- ClickhouseClient.py
+++ ClickhouseClient.py
@@ -1,55 +1,56 @@
 from clickhouse_driver import Client
 
-# Queries for databases
-def showDatabases():
-    # Return databases in clickhouse server
-    client = Client('10.0.0.22')
-    query = "SHOW DATABASES;"
-    return client.execute(query)
+class ClickhouseClient:
 
-def dropDatabase():
-    # Drop the database
-    client = Client('10.0.0.22')
-    query = "DROP DATABASE system;"
-    client.execute(query)
-    print("Database is dropped successfully")
+    def __init__(self, ipAddress):
+        self.ipAddress = ipAddress
 
-# Queries for tables
-def showTables():
-    # Return tables in clickhouse server
-    client = Client('10.0.0.22')
-    query = "SHOW TABLES;"
-    return client.execute(query)
+    # Queries for databases
+    def showDatabases(self):
+        # Return databases in clickhouse server
+        client = Client(self.ipAddress)
+        query = "SHOW DATABASES;"
+        return client.execute(query)
 
-def createTable(tableName):
-    # Create a new table in clickhouse server
-    client = Client('10.0.0.22')
-    query = 'CREATE TABLE {} (user_id String, user_name String, tweet_id String, text String) ENGINE = Memory'.format(tableName)
-    client.execute(query)
-    print("Table is created successfully")
+    def dropDatabase(self):
+        # Drop the database
+        client = Client(self.ipAddress)
+        query = "DROP DATABASE system;"
+        client.execute(query)
+        print("Database is dropped successfully")
 
-def insertData(tableName,userID,userName,tweetID,text):
-    # Inserting the data into the table
-    client = Client('10.0.0.22')
-    query = 'INSERT INTO {} (user_id,user_name,tweet_id,text) VALUES'.format(tableName)
-    client.execute(query,[{'user_id':userID,'user_name':userName,'tweet_id':tweetID,'text':text}])
-    print('Record is added successfully')
+    # Queries for tables
+    def showTables(self):
+        # Return tables in clickhouse server
+        client = Client(self.ipAddress)
+        query = "SHOW TABLES;"
+        return client.execute(query)
 
-def selectData(tableName):
-    # Retrive table data
-    client = Client('10.0.0.22')
-    query = 'SELECT * FROM {};'.format(tableName)
-    return client.execute(query)
+    def createTable(self,tableName):
+        # Create a new table in clickhouse server
+        client = Client(self.ipAddress)
+        query = 'CREATE TABLE {} (user_id String, user_name String, tweet_id String, text String) ENGINE = Memory'.format(tableName)
+        client.execute(query)
+        print("Table is created successfully")
 
-def dropTable(tableName):
-    # Drop the table
-    client = Client('10.0.0.22')
-    query = 'DROP TABLE IF EXISTS {};'.format(tableName)
-    client.execute(query)
-    print("Table is dropped successfully")
+    def insertData(self,tableName,userID,userName,tweetID,text):
+        # Inserting the data into the table
+        client = Client(self.ipAddress)
+        query = 'INSERT INTO {} (user_id,user_name,tweet_id,text) VALUES'.format(tableName)
+        client.execute(query,[{'user_id':userID,'user_name':userName,'tweet_id':tweetID,'text':text}])
+        print('Record is added successfully')
 
-###################################################
+    def selectData(self,tableName):
+        # Retrive table data
+        client = Client(self.ipAddress)
+        query = 'SELECT * FROM {};'.format(tableName)
+        return client.execute(query)
 
-
+    def dropTable(self,tableName):
+        # Drop the table
+        client = Client(self.ipAddress)
+        query = 'DROP TABLE IF EXISTS {};'.format(tableName)
+        client.execute(query)
+        print("Table is dropped successfully")
 
 
