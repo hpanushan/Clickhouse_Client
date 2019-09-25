4a797898ffd8b713dd8aad1220a04bf795f45f46
anushanramesh <anushan@globalwavenet.com>
2019-09-25 12:30:24 +0530

    A method is added to drop all tables from database

--- Main.py
+++ Main.py
@@ -4,8 +4,7 @@ from ClickhouseClient import *
 def main():
     obj = ClickhouseClient("10.0.0.30")
     #print(obj.selectData('facebook','F_102699651071355_105714230769897_20190810_20190931'))
-    obj.createTable("twitter","test")
-    obj.insertData("twitter","test","dsds","122","Anushan","1234","testing this")
+    obj.dropAllTables("twitter")
     
 if __name__ == '__main__':
     main()
